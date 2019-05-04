fe7f617105fda4c476ceabf65b8907c8d2b5be33
Karolina Adamczyk <karolina.adamczyk21@gmail.com>
2019-05-05 01:46:46 +0200

    connection to server added

--- script.js
+++ script.js
@@ -1,113 +1,135 @@
-var initialList = ['shopping', 'reports', 'pay bills', 'doctor appointment', 'pay ticket', 'sent documents'];
-var $taskList;
-var $newDoElement;
-var $addTaskButton;
+   //var initialList = ['shopping', 'reports', 'pay bills', 'doctor //appointment', 'pay ticket', 'sent documents'];
+   var $taskList;
+   var $newDoElement;
+   var $addTaskButton;
 
-function main() {
-	$taskList = document.getElementById('task-list');
-	$newDoElement = document.getElementById('add-tasks');
-	$addTaskButton = document.getElementById('add');
+   function main() {
 
-	$addTaskButton.addEventListener('click', addButtonClickHandler);
-	$taskList.addEventListener('click', listClickManager);
+   	$taskList = document.getElementById('task-list');
+   	$newDoElement = document.getElementById('add-tasks');
+   	$addTaskButton = document.getElementById('add');
 
-	initialList.forEach((element) => {
-		addListElement($taskList, element)
-	})
-}
-
-function listClickManager(event) {
-	if (event.target.classList.contains('edit')) {
-		editClickHandler(event);
-	} else if (event.target.classList.contains('accept')) {
-		acceptChangeHandler(event);
-	} else {
-		if (event.target.tagName === 'LI') {
-			event.target.classList.toggle('alternativeTask');
-		} else {
-			event.target.parentElement.classList.toggle('alternative');
-		}
-	}
-}
-
-function acceptChangeHandler(event) {
-	var $changeInput = event.target.parentElement.getElementsByTagName('input')[0];
-	var $acceptButton = event.target.parentElement.getElementsByClassName('accept')[0];
-	$changeInput.style.display = 'none';
-	$acceptButton.style.display = 'none';
-
-	var $textElement = event.target.parentElement.getElementsByTagName('span')[0];
-	var $editButton = event.target.parentElement.getElementsByClassName('edit')[0];
-	$textElement.textContent = $changeInput.value;
-	$textElement.style.display = '';
-	$editButton.style.display = 'inline-block';
-}
-
-function editClickHandler(event) {
-	var $textElement = event.target.parentElement.getElementsByTagName('span')[0];
-	var $changeInput = event.target.parentElement.getElementsByTagName('input')[0];
-	var $acceptButton = event.target.parentElement.getElementsByClassName('accept')[0];
-	var oldTask = $textElement.textContent;
-	$textElement.style.display = 'none';
-	event.target.style.display = 'none';
-	$changeInput.value = oldTask;
-	$changeInput.style.display = 'inline-block';
-	$acceptButton.style.display = 'inline-block';
-
-}
-
-function myFunction() {
-
-	var checkBox = document.getElementById("myCheck");
-
-	var text = document.getElementById("text");
+   	$addTaskButton.addEventListener('click', addButtonClickHandler);
+   	$taskList.addEventListener('click', listClickManager);
 
 
-	if (checkBox.checked == true) {
-		text.style.display = "block";
-	} else {
-		text.style.display = "none";
-	}
-}
+
+   	asyncAwaitGetTodos();
+   	getTodos();
+   }
+
+   function getTodos() {
+   	axios('http://195.181.210.249:3000/todo/')
+   		.then(response => response.data)
+   		.then(data => {
+   			data.forEach(element => {
+   				addListElement($taskList, element.title, element.id)
+   			});
+   		})
+   		.catch(console.error)
+   }
+
+   async function asyncAwaitGetTodos() {
+   	try {
+   		const response = await axios('http://195.181.210.249:3000/todo/');
+
+   		response.data.forEach(element => {
+   			addListElement($taskList, element.title, element.id)
+   		});
+   	} catch (e) {
+   		console.error(e);
+   	}
+   }
+
+   function listClickManager(event) {
+   	if (event.target.classList.contains('edit')) {
+   		editClickHandler(event);
+   	} else if (event.target.classList.contains('accept')) {
+   		acceptChangeHandler(event);
+   	} else {
+   		if (event.target.tagName === 'LI') {
+   			event.target.classList.toggle('alternativeTask');
+   		} else {
+   			event.target.parentElement.classList.toggle('alternative');
+   		}
+   	}
+   }
+
+   function acceptChangeHandler(event) {
+   	var $changeInput = event.target.parentElement.getElementsByTagName('input')[0];
+   	var $acceptButton = event.target.parentElement.getElementsByClassName('accept')[0];
+   	$changeInput.style.display = 'none';
+   	$acceptButton.style.display = 'none';
+
+   	var $textElement = event.target.parentElement.getElementsByTagName('span')[0];
+   	var $editButton = event.target.parentElement.getElementsByClassName('edit')[0];
+   	$textElement.textContent = $changeInput.value;
+   	$textElement.style.display = '';
+   	$editButton.style.display = 'inline-block';
+   }
+
+   function editClickHandler(event) {
+   	var $textElement = event.target.parentElement.getElementsByTagName('span')[0];
+   	var $changeInput = event.target.parentElement.getElementsByTagName('input')[0];
+   	var $acceptButton = event.target.parentElement.getElementsByClassName('accept')[0];
+   	var oldTask = $textElement.textContent;
+   	$textElement.style.display = 'none';
+   	event.target.style.display = 'none';
+   	$changeInput.value = oldTask;
+   	$changeInput.style.display = 'inline-block';
+   	$acceptButton.style.display = 'inline-block';
+
+   }
 
 
-function addListElement(list, listTask) {
-	var newElement = document.createElement('li');
-	var textElement = document.createElement('span');
-
-	textElement.textContent = listTask;
-
-	var editButton = document.createElement('button');
-
-	editButton.textContent = 'edit task';
-	editButton.classList.add('edit');
-
-	var acceptButton = document.createElement('button');
-
-	acceptButton.textContent = 'accept';
-	acceptButton.classList.add('accept');
-	acceptButton.style.display = 'none';
-
-	var changeInput = document.createElement('input');
-	changeInput.style.display = 'none';
-
-	newElement.appendChild(textElement);
-	newElement.appendChild(changeInput);
-
-	newElement.appendChild(acceptButton);
-	newElement.appendChild(editButton);
 
 
-	list.appendChild(newElement);
-}
+   function addListElement(list, listTask) {
+   	var newElement = document.createElement('li');
+   	var textElement = document.createElement('span');
 
-function addButtonClickHandler() {
-	var newTask = $newDoElement.value;
+   	textElement.textContent = listTask;
 
-	if (newTask) {
-		addListElement($taskList, newTask);
-		$newDoElement.value = '';
-	}
-}
+   	var editButton = document.createElement('button');
 
-document.addEventListener('DOMContentLoaded', main);
+   	editButton.textContent = 'edit task';
+   	editButton.classList.add('edit');
+
+   	var acceptButton = document.createElement('button');
+
+   	acceptButton.textContent = 'accept';
+   	acceptButton.classList.add('accept');
+   	acceptButton.style.display = 'none';
+
+   	var changeInput = document.createElement('input');
+   	changeInput.style.display = 'none';
+
+   	newElement.appendChild(textElement);
+   	newElement.appendChild(changeInput);
+
+   	newElement.appendChild(acceptButton);
+   	newElement.appendChild(editButton);
+
+
+   	list.appendChild(newElement);
+   }
+
+   function addButtonClickHandler() {
+   	var newTask = $newDoElement.value;
+
+   	if (newTask) {
+   		axios.post('http://195.181.210.249:3000/todo/', {
+   				title: newTask,
+   				author: 'Karolina'
+   			})
+   			.then(function () {
+   				$taskList.innerHTML = '';
+   				getTodos();
+   			})
+
+   		//addListElement($taskList, newTask);
+   		$newDoElement.value = '';
+   	}
+   }
+
+   document.addEventListener('DOMContentLoaded', main);
